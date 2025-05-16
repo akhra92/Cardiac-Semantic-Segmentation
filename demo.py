ecf1414171280439280f9c4c10f043e40bf78522
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-16 09:49:09 +0900

    Modified demo.py file

--- demo.py
+++ demo.py
@@ -2,12 +2,61 @@ import torch
 import argparse
 import streamlit as st
 from utils import transforms
+import torchvision.transforms as T
 from model import UNet
-import _timm
+import timm
 import config as cfg
 from PIL import Image, ImageFont
 st.set_page_config(layout='wide')
 
+
 def load_model(num_classes, checkpoint_path):
-    m = UNet(in_channels=3, out_channels=64, num_classes=cfg.NUM_CLASSES, up_method='tr_conv')
+    m = UNet(in_channels=3, out_channels=64, num_classes=num_classes, up_method='tr_conv')
+    m.load_state_dict(torch.load(checkpoint_path, map_location='cpu'))
+
+    return m.eval()
+
+
+def tn_2_np(t):
+    invTrans = T.Compose([
+        T.Normalize(mean=[0., 0., 0.], std=[1/0.229, 1/0.224, 1/0.225]),
+        T.Normalize(mean=[-0.485, -0.456, -0.406], std=[1., 1., 1.])
+    ])
+    rgb = True if len(t.shape) == 3 else False
+    return (invTrans(t) * 255).detach().cpu().permute(1, 2, 0).numpy().astype(np.uint8) if rgb else (t * 255).detach().cpu().numpy().astype(np.uint8)
+
+
+def predict(m, path, tfs):
+    im = Image.open(path)
+    im.save(path)
+
+    return im, torch.max(m(tfs(im).unsqueeze(0)).data, 1)[1]
+
+
+def run(args):
+    tfs = transforms()
+    
+    default_path = './sample_images/image25.jpg'
+
+    m = load_model(cfg.NUM_CLASSES, args.checkpoint_path)
+    st.title('Medical Image Segmentation')
+    file = st.file_uploader()
+    im, pred = predict(m =m, path=file, tfs=tfs) if file else predict(m=m, path=default_path, tfs=tfs)
+    st.write(f'Input Image: ')
+    st.image(im)
+    pred = tn_2_np(pred.squeeze(0))
+    st.write(f'Output Mask: ')
+    st.image(Image.open(pred))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Medical Image Segmentation Demo')
+
+    parser.add_argument('-cp', '--checkpoint_path', type=str, default='./saved_models/best_model.pth')
+
+    args = parser.parse_args()
+
+    run(args)
+
 
